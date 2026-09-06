eed55bd95bdb1bba1ff796bce57b547a84e617cb
ezzeldinrashad197-sudo <ezzeldinrashad197@gmail.com>
2026-09-06 09:15:40 +0300

    Update calculations.ts

--- src/utils/calculations.ts
+++ src/utils/calculations.ts
@@ -339,5 +339,6 @@ export const getClosedOpenByDocType = (
   const open = docType === 'NCR' || docType === 'SOR'
     ? (s.rejectedOpen || 0)
     : (s.rejectedOpen || 0) + (s.pending || 0);
-
+return { closed, open };
+};
   
